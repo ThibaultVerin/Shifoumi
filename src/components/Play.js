592c94e43cf4f1855c79c0003f50c9c56cc3020a
Thibault Verin <thibaultverin@MacBook-Air-de-Thibault.home>
2020-12-02 10:37:56 +0100

    Counter done

--- src/components/Play.js
+++ src/components/Play.js
@@ -14,43 +14,54 @@ const Play = () => {
     const [playerChoice, setPlayerChoice] = useState(null);
     const [computerChoice, setComputerChoice] = useState(null);
     const [isWinner, setIsWinner] = useState('');
-    //const [playerCounter, setPlayerCounter] = useState
+    const [playerCounter, setPlayerCounter] = useState(0);
+    const [computerCounter, setComputerCounter] = useState(0);
 
     const randomPick = () => {
         setComputerChoice(Math.floor(Math.random() * 3));
         console.log(playerChoice);
         console.log(Math.floor(Math.random() * 3));
+    }
+
+    const comparePick = () => {
         if (playerChoice === computerChoice){
-            setIsWinner('Draw')
+            setIsWinner('Draw');
             console.log('Draw')
         }else if ((playerChoice === 0 && computerChoice === 2) || (playerChoice === 1 && computerChoice === 0) || (playerChoice === 2 && computerChoice === 1)){
-            setIsWinner("You win")
+            setIsWinner("You win");
+            setPlayerCounter(playerCounter + 1)
             console.log("You win")
         } else{
-            setIsWinner("You loose")
+            setIsWinner("You loose");
+            setComputerCounter(computerCounter + 1);
             console.log("You loose")
         }
+    }
+
+    const result = async () => {
+        await randomPick();
+        comparePick();
     }
 
     return(
         <div className='playBody'>
             <h1>Challenge me if you dare !</h1>
-            <div>{isWinner}</div>
+            <h2>{isWinner}</h2>
             <div className='playPart'>
                 <div className="playerChoice">
-                    <img src={paper} alt={paper} className="playerWeapon" onClick={() => {setPlayerChoice(0); randomPick()}} />
-                    <img src={cisors} alt={cisors} onClick={() => {setPlayerChoice(1); randomPick()}} />
-                    <img src={rock} alt={rock} onClick={() => {setPlayerChoice(2); randomPick()}} />
+                    <img src={paper} alt={paper} className="playerWeapon" onClick={() => {setPlayerChoice(0); result()}} />
+                    <img src={cisors} alt={cisors} onClick={() => {setPlayerChoice(1); result()}} />
+                    <img src={rock} alt={rock} onClick={() => {setPlayerChoice(2); result()}} />
                 </div>
                 <img src={doctor} alt={doctor} className='avatarimg'  style={{width: 300, height: 400, position: 'absolute', left: 50, bottom: 150, borderRadius:20}} />
-                <div className="scorePartDoctor"><h2>Score :</h2></div>
+                <div className="scorePartDoctor"><h2>Score : {playerCounter}</h2></div>
                 <div className="computerChoice">
                     <img src={paperReverse} alt={paperReverse} className="playerWeapon"/>
                     <img src={cisorsReverse} alt={cisorsReverse} />
                     <img src={rockReverse} alt={rockReverse} />
                 </div>
                 <img src={jesus} alt={jesus} style={{width: 300, height: 400, position: 'absolute', right: 50, bottom: 150, borderRadius: 20 }}/>
-                <div className="scorePartJesus"><h2>Score :</h2></div>
+                <div className="scorePartJesus"><h2>Score : {computerCounter}</h2></div>
             </div>
             
         </div>
@@ -72,3 +83,8 @@ export default Play;
 // Step 3 : définir en fonction des 2 states liés aux choix 
 // -> création d'un state isWinner pour savoir si l'utilisateur gagne
 // -> création conditions 
+
+// Step 4 : créer counter:
+// -> creer state pour player et computer
+// -> incrémente de 1 dans les conditions
+// -> affichage h2 dans le return
