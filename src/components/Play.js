764926220af128b5c88e363eb3aa4ec5bf68a33d
Thibault Verin <thibaultverin@MacBook-Air-de-Thibault.local>
2020-12-02 07:40:57 +0100

    Algo done

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Play.css';
 import paper from '../Images/paper.png';
 import cisors from '../Images/cisors.png';
@@ -10,14 +10,37 @@ import jesus from '../Images/jesus.jpg';
 import doctor from '../Images/doctor.jpg';
 
 const Play = () => {
+
+    const [playerChoice, setPlayerChoice] = useState(null);
+    const [computerChoice, setComputerChoice] = useState(null);
+    const [isWinner, setIsWinner] = useState('');
+    //const [playerCounter, setPlayerCounter] = useState
+
+    const randomPick = () => {
+        setComputerChoice(Math.floor(Math.random() * 3));
+        console.log(playerChoice);
+        console.log(Math.floor(Math.random() * 3));
+        if (playerChoice === computerChoice){
+            setIsWinner('Draw')
+            console.log('Draw')
+        }else if ((playerChoice === 0 && computerChoice === 2) || (playerChoice === 1 && computerChoice === 0) || (playerChoice === 2 && computerChoice === 1)){
+            setIsWinner("You win")
+            console.log("You win")
+        } else{
+            setIsWinner("You loose")
+            console.log("You loose")
+        }
+    }
+
     return(
         <div className='playBody'>
             <h1>Challenge me if you dare !</h1>
+            <div>{isWinner}</div>
             <div className='playPart'>
                 <div className="playerChoice">
-                    <img src={paper} alt={paper} className="playerWeapon"/>
-                    <img src={cisors} alt={cisors} />
-                    <img src={rock} alt={rock} />
+                    <img src={paper} alt={paper} className="playerWeapon" onClick={() => {setPlayerChoice(0); randomPick()}} />
+                    <img src={cisors} alt={cisors} onClick={() => {setPlayerChoice(1); randomPick()}} />
+                    <img src={rock} alt={rock} onClick={() => {setPlayerChoice(2); randomPick()}} />
                 </div>
                 <img src={doctor} alt={doctor} className='avatarimg'  style={{width: 300, height: 400, position: 'absolute', left: 50, bottom: 150, borderRadius:20}} />
                 <div className="scorePartDoctor"><h2>Score :</h2></div>
@@ -29,8 +52,23 @@ const Play = () => {
                 <img src={jesus} alt={jesus} style={{width: 300, height: 400, position: 'absolute', right: 50, bottom: 150, borderRadius: 20 }}/>
                 <div className="scorePartJesus"><h2>Score :</h2></div>
             </div>
+            
         </div>
     )
 }
 
 export default Play;
+
+
+// Step 1 : Reconnaitre le click:
+// -> creation state choix du player
+// -> onClick={() => setPlayerChoice()} pour mettre à jour le state.
+
+// Step 2 : Au click donner un state d'une valeur aléatoire pour l'ordi
+// -> creer state computer
+// -> creer un random number entre 0 et 2 pour le choix de l'ordi --> console.log en premier puis mettre à jour le state.
+// -> Math.floor(Math.random() * Math.floor(3))
+
+// Step 3 : définir en fonction des 2 states liés aux choix 
+// -> création d'un state isWinner pour savoir si l'utilisateur gagne
+// -> création conditions 
